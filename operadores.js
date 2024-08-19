450acb8f65c378b284d225bf875b650834b32b85
the-raven-an <the-raven-an@gmail.com>
2024-08-19 20:31:22 +0200

    Operadores en JavaScript

---
+++ operadores.js
@@ -0,0 +1,27 @@
+//Suma
+console.log(3 + 5);
+
+//Resta
+console.log(14 - 7);
+
+//Multiplicacion
+console.log(8 * 6);
+
+//Division
+console.log(30 / 9);
+
+//Resto
+console.log(95 % 4);
+
+//Prueba de precedencia
+console.log(3 + 5 * 2 - 8);
+
+//Aplicar ()
+console.log((3 + 5) * 2 - 8);
+
+//Concatenar Strings
+
+console.log('Hola ' + 'TheRaven.');
+var nombre = 'TheRaven';
+var saludo = 'Hola ' + nombre;
+console.log(saludo + '.')
