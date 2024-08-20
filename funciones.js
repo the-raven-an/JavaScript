b5bf5ba20a98f846f1a98df25511a0db1e821323
the-raven-an <the-raven-an@gmail.com>
2024-08-20 12:49:36 +0200

    Funciones básicas en JavaScript

---
+++ funciones.js
@@ -0,0 +1,15 @@
+//Método 1 para declarar funciones:
+function sumaTres(x) {
+	return (x + 3)
+}
+
+//Método 2 para declarar funciones:
+var sumaTres = function (x) { //almacenamos el valor de la variable sumaTres
+    return x + 3;
+} 
+
+//Método 3 para declarar funciones:
+var sumaTres = (x) => { //se denomina también como método de la flecha
+    return x + 3;
+  }
+  
