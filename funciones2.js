84ad14c1381ef7bdf1aecc6e14849bc1ad2dd357
the-raven-an <the-raven-an@gmail.com>
2024-08-20 12:49:58 +0200

    Uso de console.log y return

---
+++ funciones2.js
@@ -0,0 +1,19 @@
+//Explicación del uso de console.log y return
+function cuidadoConElConsoleLog(nombre) {
+    console.log(nombre); //muestra en la consola el valor entre paréntesis, pero no lo regresa al código, sólo se usa como ayuda visual para el programador.
+    return nombre; //regresa a la línea de código el valor y el tipo de valor que es, como por ejemplo un string que se usatá en otras partes del código.
+  }
+
+  //Aquí el programa nos va a retornar el texto entrecomillado más el tipo de valor a la función cuidadoConElConsoleLog
+  function cuidadoConElReturn(nombre) {
+    return (
+        "El nombre retornado por la función cuidadoConElConsoleLog es " + cuidadoConElConsoleLog('TheRaven')
+    ); 
+
+  }
+
+  function finDeLaFuncion(nombre) {
+    return nombre;
+    console.log(nombre);
+    //Cuando se ejecuta el return, el resto de operaciones quedan sin realizar, por ello, sólo se mostraría el return en consola, y no el console.log
+  }
